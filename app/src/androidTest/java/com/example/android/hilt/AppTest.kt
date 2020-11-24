2a2d76907516a4e36c49bc076657529c18750ee7
Neil Ling <neil.ling@aylaasia.com>
2020-11-24 19:49:34 +0800

    refactor code to remove dependency to service locator

--- app/src/androidTest/java/com/example/android/hilt/AppTest.kt
+++ app/src/androidTest/java/com/example/android/hilt/AppTest.kt
@@ -20,24 +20,25 @@ import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
+import com.example.android.hilt.data.LoggerLocalDataSource
 import com.example.android.hilt.ui.MainActivity
 import org.hamcrest.Matchers.containsString
 import org.junit.After
 import org.junit.Test
 import org.junit.runner.RunWith
+import javax.inject.Inject
 
 @RunWith(AndroidJUnit4::class)
 class AppTest {
 
+    @Inject lateinit var loggerLocalDataSource: LoggerLocalDataSource
+
     @After
     fun tearDown() {
         // Remove logs after the test finishes
-        ServiceLocator(getInstrumentation().targetContext).loggerLocalDataSource.removeLogs()
+        loggerLocalDataSource.removeLogs()
     }
 
     @Test
